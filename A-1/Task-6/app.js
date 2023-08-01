61d96653aa7d00d9480fa8eb22d359c7cb873440
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-08-01 20:37:01 +0530

    commit

--- A-1/Task-6/app.js
+++ A-1/Task-6/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log('Mudassir_Quarishi')
+    console.log('Mudassir Quarishi')
 });
 
 server.listen(4000);
