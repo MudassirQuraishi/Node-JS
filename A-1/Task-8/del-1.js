10f4c5e353eb7a741e974d242675756f6a55c993
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-08-05 22:34:40 +0530

    saving data from request

--- A-1/Task-8/del-1.js
+++ A-1/Task-8/del-1.js
@@ -5,31 +5,21 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === '/') {
-    fs.readFile('message.txt',(err, fileContent) => {
-      res.write('<html>');
-      res.write('<head><title>Enter Message</title><head>');
-      res.write('<body>');
-      res.write('<div class="messages">');
-
-      if (!err && fileContent) {
-        res.write('<p>Messages:</p>');
-        res.write(`<p>${fileContent}</p>`);
-      }
-      else if(!fileContent){
-        res.write('<p>No messages</p>');
-      }
-
-      res.write('</div>');
-      res.write('<form action="/message" method="POST">');
-      res.write('<input type="text" name="message" placeholder="Enter a message">');
-      res.write('<button type="submit">Send</button></form>');
-      res.write('</body>');
-      res.write('</html>');
-      res.end();
-    });
-  } else if (url === '/message' && method === 'POST') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body>');
+    res.write('<div class = "messages"> </div>')
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text" name="message">') 
+    res.write('<button type="submit" onClick ="readFile()">Send</button></form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
+      console.log(chunk);
       body.push(chunk);
     });
     return req.on('end', () => {
@@ -41,14 +31,13 @@ const server = http.createServer((req, res) => {
         return res.end();
       });
     });
-  } else {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-  }
+  };
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
-server.listen(1000);
+server.listen(3000);
